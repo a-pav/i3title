f2f167b8e273ce88f51e54b37f8b0efc42dbdeac
a-pav <14939129+a-pav@users.noreply.github.com>
2023-01-31 21:53:10 +0330

    Use [][]string for filters

--- main.go
+++ main.go
@@ -10,27 +10,23 @@ import (
 	"go.i3wm.org/i3/v4"
 )
 
-type ReReplace struct {
-	Re   *regexp.Regexp
-	Repl string
+type MatchReplace struct {
+	Match *regexp.Regexp
+	Repl  string
 }
 
 var (
-	args0   string // program pathname
 	titlefi *os.File
 	config  = struct {
-		LogTitle  bool `json:"log_title"`
-		CharLimit int  `json:"char_limit"`
-		Filters   []struct {
-			Old string `json:"old"`
-			New string `json:"new"`
-		} `json:"filters"`
+		Debug   bool       `json:"debug"`
+		MaxLen  int        `json:"max_length"`
+		Filters [][]string `json:"filters"`
 
-		FiltersCompiled []ReReplace
+		FiltersCompiled []MatchReplace
 	}{}
 )
 
-func initReadConfig() {
+func initReadConfig(args0 string) {
 	// read config file from '<program-name>.config.json'
 	bs, err := os.ReadFile(args0 + ".config.json")
 	if err != nil {
@@ -44,25 +40,29 @@ func initReadConfig() {
 		log.Fatal("reading config file: ", err)
 	}
 
-	filtersCompiled := []ReReplace{}
-	for _, f := range config.Filters {
-		re, err := regexp.Compile(f.Old)
+	filtersCompiled := []MatchReplace{}
+	for _, mr := range config.Filters {
+		if l := len(mr); l != 2 {
+			continue
+		}
+
+		re, err := regexp.Compile(mr[0])
 		if err != nil {
 			log.Println("error compiling regex:", err)
 			continue
 		}
 
-		filtersCompiled = append(filtersCompiled, ReReplace{
-			Re:   re,
-			Repl: f.New,
+		filtersCompiled = append(filtersCompiled, MatchReplace{
+			Match: re,
+			Repl:  mr[1],
 		})
 	}
 
 	config.FiltersCompiled = filtersCompiled
-	config.Filters = nil // discard non-compiled filters
+	config.Filters = nil // discard
 }
 
-func initOpenTitleFile() {
+func initOpenTitleFile(args0 string) {
 	// open file for writing title at '<program-name>.out'
 	fi, err := os.Create(args0 + ".out")
 	if err != nil {
@@ -72,9 +72,10 @@ func initOpenTitleFile() {
 }
 
 func init() {
-	args0 = os.Args[0]
-	initReadConfig()
-	initOpenTitleFile()
+	args0 := os.Args[0]
+
+	initReadConfig(args0)
+	initOpenTitleFile(args0)
 }
 
 func main() {
@@ -92,7 +93,7 @@ func main() {
 
 func writeTitle(title string) {
 	for _, filter := range config.FiltersCompiled {
-		title = filter.Re.ReplaceAllString(title, filter.Repl)
+		title = filter.Match.ReplaceAllString(title, filter.Repl)
 	}
 
 	// convert title string to runes, because unicode characters can have length > 1
@@ -102,29 +103,29 @@ func writeTitle(title string) {
 	//	 fmt.Println(len(str)) // prints 5
 	//	 fmt.Println(len([]rune(str))) // prints 2
 	titleRunes := []rune(title)
-	if len(titleRunes) > config.CharLimit {
-		title = string(titleRunes[:config.CharLimit]) + "..."
+	if len(titleRunes) > config.MaxLen {
+		title = string(titleRunes[:config.MaxLen]) + "..."
 	}
 
-	if err := titlefi.Truncate(0); err != nil {
+	if err := titlefi.Truncate(0); config.Debug && err != nil {
 		log.Println("truncating 'window-title' file:", err)
 	}
-	if _, err := titlefi.Seek(0, 0); err != nil {
+	if _, err := titlefi.Seek(0, 0); config.Debug && err != nil {
 		log.Println("seeking 'window-title' file:", err)
 
 	}
-	if _, err := titlefi.Write([]byte(title)); err != nil {
+	if _, err := titlefi.Write([]byte(title)); config.Debug && err != nil {
 		log.Println("writing 'window-title' file:", err)
 	}
 
-	switch config.LogTitle {
+	switch config.Debug {
 	case true:
 		go log.Printf("%q", title)
 	}
 }
 
 func i3StatusRefresh() {
-	if err := exec.Command("killall", "-USR1", "i3status").Run(); err != nil {
+	if err := exec.Command("killall", "-USR1", "i3status").Run(); config.Debug && err != nil {
 		log.Println("error refreshing i3status:", err)
 	}
 }
