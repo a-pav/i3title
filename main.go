7342fbcb4a042bff0b84092b488c3d1a6c154bca
a-pav <14939129+a-pav@users.noreply.github.com>
2023-09-25 15:34:15 +0330

    Refactor and Cleanups

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"regexp"
+	"strings"
+	"sync"
 
 	"go.i3wm.org/i3/v4"
 )
@@ -16,17 +19,29 @@ type MatchReplace struct {
 }
 
 var (
-	titlefi *os.File
-	config  = struct {
-		Debug   bool       `json:"debug"`
-		MaxLen  int        `json:"max_length"`
+	scanner *bufio.Scanner
+
+	// Globally shared variables.
+	// For the sake of this program, these should work fine with no mutex mechanism in place.
+	TITLE = ""
+	LINE  = ""
+
+	config = struct {
+		Debug       bool `json:"debug"`
+		TitleModule struct {
+			Index      int    `json:"index"`
+			Format     string `json:"format"`
+			MaxLen     int    `json:"max_length"`
+			WelcomeMsg string `json:"welcome_msg"` // It is shown until the first window/title event is triggered.
+		} `json:"title_module"`
+
 		Filters [][]string `json:"filters"`
 
 		FiltersCompiled []MatchReplace
 	}{}
 )
 
-func initReadConfig(args0 string) {
+func readConfig(args0 string) {
 	// read config file from '<program-name>.config.json'
 	bs, err := os.ReadFile(args0 + ".config.json")
 	if err != nil {
@@ -39,6 +54,16 @@ func initReadConfig(args0 string) {
 	if err := json.Unmarshal(bs, &config); err != nil {
 		log.Fatal("reading config file: ", err)
 	}
+}
+
+// discardConfig discards parts of the config that are no longer needed.
+func discardConfig() {
+	config.Filters = nil // discard
+	config.TitleModule.WelcomeMsg = ""
+}
+
+func initConfig(args0 string) {
+	readConfig(args0)
 
 	filtersCompiled := []MatchReplace{}
 	for _, mr := range config.Filters {
@@ -59,73 +84,105 @@ func initReadConfig(args0 string) {
 	}
 
 	config.FiltersCompiled = filtersCompiled
-	config.Filters = nil // discard
 }
 
-func initOpenTitleFile(args0 string) {
-	// open file for writing title at '<program-name>.out'
-	fi, err := os.Create(args0 + ".out")
-	if err != nil {
-		log.Fatal("could not open/create window-title file:", err)
-	}
-	titlefi = fi
+func initScanner() {
+	scanner = bufio.NewScanner(os.Stdin)
+
+	scanner.Scan() // skip 1st line: {"version":1}
+	fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
+	scanner.Scan() // skip 2nd line: [
+	fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
+	scanner.Scan() // skip 3rd line: the only line without ',' as delimiter
+	fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
 }
 
 func init() {
+	defer discardConfig()
+
 	args0 := os.Args[0]
 
-	initReadConfig(args0)
-	initOpenTitleFile(args0)
+	initConfig(args0)
+	initScanner()
+
+	TITLE = config.TitleModule.WelcomeMsg
 }
 
 func main() {
-	winRecv := i3.Subscribe(i3.WindowEventType)
-	for winRecv.Next() {
-		ev := winRecv.Event().(*i3.WindowEvent)
-		writeTitle(ev.Container.WindowProperties.Title)
-		i3StatusRefresh()
+	wg := sync.WaitGroup{}
 
-		// break
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	log.Fatal("ending program:", winRecv.Close())
+		winRecv := i3.Subscribe(i3.WindowEventType)
+		for winRecv.Next() {
+			ev := winRecv.Event().(*i3.WindowEvent)
+			TITLE = trimTitle(ev.Container.WindowProperties.Title)
+
+			// There's no need to signal i3status to refresh. It picks on the stdout by itself.
+			writeStdOut()
+
+			// break
+		}
+
+		log.Fatal("ending program:", winRecv.Close())
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		for scanner.Scan() {
+			LINE = scanner.Text()
+			// line = scanner.Text()
+			writeStdOut()
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal("scanner error:", err)
+		}
+	}()
+
+	wg.Wait()
 }
 
-func writeTitle(title string) {
+// trimTitle applies the defined filters, maxlen, etc.
+func trimTitle(title string) string {
 	for _, filter := range config.FiltersCompiled {
 		title = filter.Match.ReplaceAllString(title, filter.Repl)
 	}
 
-	// convert title string to runes, because unicode characters can have length > 1
+	// Convert title string to runes, because unicode characters can have length > 1
 	// when they're actually one single rune.
 	// example:
 	//	 str := "·—"
 	//	 fmt.Println(len(str)) // prints 5
 	//	 fmt.Println(len([]rune(str))) // prints 2
 	titleRunes := []rune(title)
-	if len(titleRunes) > config.MaxLen {
-		title = string(titleRunes[:config.MaxLen]) + "..."
-	}
-
-	if err := titlefi.Truncate(0); config.Debug && err != nil {
-		log.Println("truncating 'window-title' file:", err)
-	}
-	if _, err := titlefi.Seek(0, 0); config.Debug && err != nil {
-		log.Println("seeking 'window-title' file:", err)
-
-	}
-	if _, err := titlefi.Write([]byte(title)); config.Debug && err != nil {
-		log.Println("writing 'window-title' file:", err)
+	if len(titleRunes) > config.TitleModule.MaxLen {
+		title = string(titleRunes[:config.TitleModule.MaxLen]) + "..."
 	}
 
 	switch config.Debug {
 	case true:
 		go log.Printf("%q", title)
 	}
+
+	return title
 }
 
-func i3StatusRefresh() {
-	if err := exec.Command("killall", "-USR1", "i3status").Run(); config.Debug && err != nil {
-		log.Println("error refreshing i3status:", err)
+// writeStdOut places `TITLE` into the coming stdin/`LINE` then writes it to stdout.
+func writeStdOut() {
+	sm := []map[string]any{}
+	if err := json.Unmarshal([]byte(strings.TrimPrefix(LINE, ",")), &sm); err != nil {
+		log.Fatal("failure parsing line:", err)
 	}
+	sm[config.TitleModule.Index]["full_text"] = fmt.Sprintf(config.TitleModule.Format, TITLE)
+
+	j, err := json.Marshal(sm)
+	if err != nil {
+		log.Fatal("failure encoding line:", err)
+	}
+	fmt.Fprintf(os.Stdout, ",%s\n", string(j))
 }
