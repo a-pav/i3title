5bd464a9fa878db6799b350dfdb0236a6716d784
a-pav <14939129+a-pav@users.noreply.github.com>
2023-10-18 17:38:57 +0330

    Simplify slicing and indexing benchmarks

--- test/slice_indexing_bench_test.go
+++ test/slice_indexing_bench_test.go
@@ -4,138 +4,60 @@ import (
 	"testing"
 )
 
+// Slicing one time.
 func BenchmarkSliceIndexingI(b *testing.B) {
-	const (
-		maxlen    = 95
-		maxesclen = 5
-	)
-	title := getBaseString()
-
+	bs := makeSlice()
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
+		x := find(bs, 'A', 3)
+		x = find(bs, 'D', 5)
+		x = find(bs, 'Z', 6)
+		x = find(bs, 'M', 12)
+		x = find(bs, 'D', 14)
 
-		if len([]rune(title)) > maxlen {
-			rs := []rune(title)                          // alloc.
-			cut := lastSafeIndexI(rs, maxlen, maxesclen) // drop half-fromed escape sequence (no alloc.)
-			cut = lastNonspaceIndexI(rs, cut) + 1        // drop trailing spaces (no alloc.)
-			rs = rs[:cut]                                // shrink (no alloc.)
-			rs = append(rs, '…')                         // append shrinkage indicator (no alloc.)
-			_ = string(rs)                               // alloc.
-
-			// b.Logf("%q\n", string(rs)) // DEBUG
-		}
+		_ = string(bs[:x])
+		// b.Logf("%q\n", string(bs[:x])) // DEBUG
 	}
-
 }
 
+// Slicing multiple times.
 func BenchmarkSliceIndexingII(b *testing.B) {
-	const (
-		maxlen    = 95
-		maxesclen = 5
-	)
-	title := getBaseString()
-
+	bs := makeSlice()
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		if len([]rune(title)) > maxlen {
-			rs := []rune(title)                      // alloc.
-			rs = rs[:maxlen]                         // shrink (no alloc.)
-			rs = rs[:lastSafeIndexII(rs, maxesclen)] // drop half-fromed escape sequence (no alloc.)
-			rs = rs[:lastNonspaceIndexII(rs)+1]      // drop trailing spaces (no alloc.)
-			rs = append(rs, '…')                     // append shrinkage indicator (no alloc.)
-			_ = string(rs)                           // alloc.
+		bsii := bs[:find(bs, 'A', 3)]
+		bsii = bsii[:find(bs, 'D', 5)]
+		bsii = bsii[:find(bs, 'Z', 6)]
+		bsii = bsii[:find(bs, 'M', 12)]
+		bsii = bsii[:find(bs, 'D', 14)]
 
-			// b.Logf("%q\n", string(rs)) // DEBUG
-
-		}
-
+		_ = string(bsii)
+		// b.Logf("%q\n", string(bsii)) // DEBUG
 	}
-
 }
 
-func lastSafeIndexI(rs []rune, cut, width int) int {
-	var (
-		end   = cut
-		start = cut - width
-
-		iSemicolon = -1
-		iAmpersand = -1
-	)
-
-	for i := end - 1; i >= start; i-- {
-		switch {
-		case iAmpersand < 0 && rs[i] == '&':
-			iAmpersand = i
-		case iSemicolon < 0 && rs[i] == ';':
-			iSemicolon = i
-		}
+func makeSlice() []byte {
+	n := 20
+	b := []byte{'A', 'B', 'C', 'D', 'X', 'Z', 'Y', 'M', 'N', 'O'}
+	bs := make([]byte, 0, n*len(b))
+	for i := 0; i < n; i++ {
+		bs = append(bs, b...)
 	}
 
-	switch {
-	case iAmpersand < iSemicolon:
-		// There's a fully formed escape sequence inside width before end. e.g. `&escape;`
-		return end
-	case iAmpersand > 0:
-		// There's a half-formed escape sequence inside width before end. e.g. `&esca`
-		return iAmpersand
-	}
-
-	// There was no escape sequence inside the width.
-	return end
+	return bs
 }
 
-func lastNonspaceIndexI(rs []rune, cut int) int {
-	for i := cut - 1; i > 0; i-- {
-		if rs[i] != ' ' {
-			return i
+func find(in []byte, f byte, count int) int {
+	for i := len(in) - 1; i > 0; i-- {
+		if in[i] == f {
+			count--
+			if count == 0 {
+				return i
+			}
 		}
 	}
 
-	// All space!
-	return 0
-}
-
-func lastSafeIndexII(rs []rune, width int) int {
-	var (
-		len0  = len(rs)
-		start = len0 - width
-		end   = len0 - 1
-
-		iSemicolon = -1
-		iAmpersand = -1
-	)
-
-	for i := end; i >= start; i-- {
-		switch {
-		case iAmpersand < 0 && rs[i] == '&':
-			iAmpersand = i
-		case iSemicolon < 0 && rs[i] == ';':
-			iSemicolon = i
-		}
-	}
-
-	switch {
-	case iAmpersand < iSemicolon:
-		// There's a fully formed escape sequence inside width before end. e.g. `&escape;`
-		return len0
-	case iAmpersand > 0:
-		// There's a half-formed escape sequence inside width before end. e.g. `&esca`
-		return iAmpersand
-	}
-
-	// There was no escape sequence inside the width.
-	return len0
-}
-
-func lastNonspaceIndexII(rs []rune) int {
-	for i := len(rs) - 1; i > 0; i-- {
-		if rs[i] != ' ' {
-			return i
-		}
-	}
-
-	// All space!
 	return 0
 }
