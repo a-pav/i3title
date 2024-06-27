8598b9b33f42a22e3988e7dedb81746d75dd6b83
a-pav <14939129+a-pav@users.noreply.github.com>
2024-06-27 18:20:02 +0330

    deploy.sh: Delay before cp and i3 restart

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,12 @@ deploy_local() {
 	go build -ldflags="-s -w" -o i3title \
 		&& killall -q i3title
 
+	sleep 0.2
 	cp ./i3title ~/.config/i3status/modules/i3title/i3title
 	cp ./config.json ~/.config/i3status/modules/i3title/config.json
+
+	sleep 0.2
+	i3-msg restart
 }
 
 deploy_local
