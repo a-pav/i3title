4e2a646fc05a310f16e982b7a480fc3aa2965e36
a-pav <14939129+a-pav@users.noreply.github.com>
2024-12-28 12:45:39 +0330

    Remove unrelated (commented) codes

--- main.go
+++ main.go
@@ -74,47 +74,27 @@ func reporter(lineCh chan []byte, titleCh, modeCh chan string) {
 	}
 }
 
-func moder(modeC chan string) {
+func moder(modeCh chan string) {
 	modeER := i3.Subscribe(i3.ModeEventType)
 
 	for modeER.Next() {
-		// e := modeER.Event().(*i3.ModeEvent)
-		// switch e.Change {
-		// case "default":
-		// 	MODE = ""
-		// 	MODE_LEN = 0
-		// default:
-		// 	switch e.PangoMarkup {
-		// 	case false:
-		// 		MODE = fmt.Sprintf(
-		// 			"<span color='red' font='italic bold'>%s</span>%s",
-		// 			e.Change, MODE_SEP,
-		// 		)
-		// 	default:
-		// 		MODE = fmt.Sprintf("%s%s", e.Change, MODE_SEP)
-		// 	}
-		// 	// == len(<mode-name>) + len(visible_sep_chars)
-		// 	MODE_LEN = len(e.Change) + MODE_SEP_LEN
-		// }
-		modeC <- modeER.Event().(*i3.ModeEvent).Change
-		// buildReport()
-		// printline()
+		modeCh <- modeER.Event().(*i3.ModeEvent).Change
 	}
 
 	log.Fatal("ending program:", modeER.Close())
 }
 
-func titler(titleC chan string) {
+func titler(titleCh chan string) {
 	// TODO: Remove: After commit 57ea2c088 there might be no need to delay.
-	if cnf.StartDelay > 0 {
-		// // i3 creates too many change-of-title events in a row while system and/or
-		// // i3 itself is initially starting. To avoid errors, it's best not to
-		// // subscribe to the events too early.
-		// REPORT = fmt.Sprintf("<i>i3title start delay: %ds</i>", cnf.StartDelay)
-		// time.Sleep(time.Duration(cnf.StartDelay) * time.Second)
-		// // Sudden empty title shuold indicate that normal operation has started.
-		// REPORT = ""
-	}
+	// if cnf.StartDelay > 0 {
+	// 	// i3 creates too many change-of-title events in a row while system and/or
+	// 	// i3 itself is initially starting. To avoid errors, it's best not to
+	// 	// subscribe to the events too early.
+	// 	REPORT = fmt.Sprintf("<i>i3title start delay: %ds</i>", cnf.StartDelay)
+	// 	time.Sleep(time.Duration(cnf.StartDelay) * time.Second)
+	// 	// Sudden empty title shuold indicate that normal operation has started.
+	// 	REPORT = ""
+	// }
 
 	windowER := i3.Subscribe(i3.WindowEventType)
 
@@ -122,22 +102,14 @@ func titler(titleC chan string) {
 		e := windowER.Event().(*i3.WindowEvent)
 		switch e.Change {
 		case "title", "focus":
-		default:
-			continue
+			titleCh <- e.Container.WindowProperties.Title
 		}
-
-		// if t := e.Container.WindowProperties.Title; t != "" && TITLE != t {
-		// 	// TITLE = t
-		titleC <- e.Container.WindowProperties.Title
-		// 	buildReport()
-		// 	printline()
-		// }
 	}
 
 	log.Fatal("ending program:", windowER.Close())
 }
 
-func liner(lineC chan []byte) {
+func liner(lineCh chan []byte) {
 	// DEBUG
 	// cmd := exec.Command("i3status")
 	// stdout, err := cmd.StdoutPipe()
@@ -158,9 +130,7 @@ func liner(lineC chan []byte) {
 	scanner.Buffer(make([]byte, 0, cnf.BufSize), 0)
 
 	for scanner.Scan() {
-		// LINE = scanner.Bytes()
-		// printline()
-		lineC <- scanner.Bytes()
+		lineCh <- scanner.Bytes()
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -189,21 +159,3 @@ func trimTitle(title string, maxlen int) string {
 
 	return replacer(title)
 }
-
-// func buildReport() {
-// 	switch MODE {
-// 	case "":
-// 		REPORT = trimTitle(TITLE, cnf.MaxLen)
-// 	default:
-// 		REPORT = MODE + trimTitle(TITLE, cnf.MaxLen-MODE_LEN)
-// 	}
-// }
-
-// // printline inserts `REPORT` into `LINE` (incoming stdin) then prints it to stdout.
-// func printline() {
-// 	fmt.Fprintf(os.Stdout, "%s\n",
-// 		// Read-only `[]byte(string)` convertions are optimized by compiler:
-// 		// https://github.com/golang/go/issues/2205 (commits=c8adb30,925d2fb,d63c88d).
-// 		bytes.Replace(LINE, []byte(cnf.PH), []byte(REPORT), 1),
-// 	)
-// }
