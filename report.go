455083d4a748230c34111584f8b9e5d9cd6d4e63
a-pav <14939129+a-pav@users.noreply.github.com>
2025-10-28 11:38:08 +0330

    Move some code

--- report.go
+++ report.go
@@ -236,3 +236,29 @@ func messagePipe(messageCh chan<- []byte) {
 		log.Println("pipe scanner error:", err)
 	}
 }
+
+// lastIndexNonSpace returns the index of last non-space character in s.
+func lastIndexNonSpace(s []rune) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] != ' ' {
+			return i
+		}
+	}
+	return -1 // All were space.
+}
+
+// Read-only `[]byte(string)` convertions are optimized by compiler:
+// https://github.com/golang/go/issues/2205 (commits=c8adb30,925d2fb,d63c88d).
+// func string2Bytes(s string) []byte {
+// 	if len(s) == 0 {
+// 		return nil
+// 	}
+// 	return unsafe.Slice(unsafe.StringData(s), len(s))
+// }
+
+// func bytes2String(b []byte) string {
+// 	if len(b) == 0 {
+// 		return ""
+// 	}
+// 	return unsafe.String(unsafe.SliceData(b), len(b))
+// }
